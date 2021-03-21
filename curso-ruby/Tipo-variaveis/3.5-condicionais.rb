72ac868ea501d5a7068d81c5579ae20e1c042e2c
Everaldo Nascimento <everaldobass@gmail.com>
2021-03-20 21:07:30 -0300

    ula de condicionais ruby

---
+++ curso-ruby/Tipo-variaveis/3.5-condicionais.rb
@@ -0,0 +1,42 @@
+# Condicionais
+
+# == igualdade
+# != Diferente
+# > Maior que
+# >= Maior ou igual
+# < Menor que
+# < Menor ou igual
+
+valor = gets.chomp.to_i
+
+if valor > 20
+    puts "Valor maior que 20"
+elsif valor == 20
+    puts "Esse valor e igual a 20"
+else
+    puts "Valor menor que 20"
+end
+
+    # ! negação
+    # && and
+    # || or
+
+# Apenas nil e false
+
+#unless Condicao
+ #   faca_isso
+#end
+
+# Ternario
+#valor >= 50?
+#puts "Eu sou maior que 50":
+#puts "Valor menor que 50"
+
+puts "Digite um valor: "
+valor = gets.chomp.to_i
+
+if unless valor == 20
+    puts "Valor igual a 20"
+    else
+    puts "Valor menor que  20"
+
